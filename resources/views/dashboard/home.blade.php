57958a582bf7b7605e8aa7c057edcc48b7284be7
hixvm <hixvmx@gmail.com>
2023-11-05 16:49:22 +0100

    create dashboard layout

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -1,1 +1,9 @@
-dashboard
+@extends('layout.dashboard')
+@section('metatags')
+    <title>Dashboard - RedStore</title>
+    <link rel="stylesheet" href="{{ asset('css/dashboard/global.css') }}" />
+@endsection
+
+@section('content')
+    Dashboard Reports
+@endsection
