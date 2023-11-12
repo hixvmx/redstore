e9125cb15b6c97a3d7ed5a449b0ab9ad55706980
hixvm <hixvmx@gmail.com>
2023-11-12 20:00:53 +0100

    show categories count

--- resources/views/dashboard/categories.blade.php
+++ resources/views/dashboard/categories.blade.php
@@ -8,7 +8,7 @@
 @section('content')
     <div class="db__sec wd__80">
         <div class="db__header">
-            <h2>التصنيفات (765)</h2>
+            <h2>التصنيفات ({{count($categories)}})</h2>
             <div class="filterOptions">
                 <button id="onlyParentsBtn">
                     <svg viewBox="0 0 24 24"><g stroke-width="0"></g><g stroke-linecap="round" stroke-linejoin="round"></g><g> <path d="M22 6C22.5523 6 23 6.44772 23 7C23 7.55229 22.5523 8 22 8H2C1.44772 8 1 7.55228 1 7C1 6.44772 1.44772 6 2 6L22 6Z"></path> <path d="M22 11C22.5523 11 23 11.4477 23 12C23 12.5523 22.5523 13 22 13H2C1.44772 13 1 12.5523 1 12C1 11.4477 1.44772 11 2 11H22Z"></path> <path d="M23 17C23 16.4477 22.5523 16 22 16H2C1.44772 16 1 16.4477 1 17C1 17.5523 1.44772 18 2 18H22C22.5523 18 23 17.5523 23 17Z"></path> </g></svg>
@@ -208,7 +208,7 @@
             </div>
         </div>
     </div>
-    
+
     <script>
         var onlyParentsBtn = document.getElementById('onlyParentsBtn');
         var ParentsAndChildrensBtn = document.getElementById('ParentsAndChildrensBtn');
