b23c97ec8b0cd2804dc16d8f91409550614a685b
hixvm <hixvmx@gmail.com>
2023-11-19 16:31:44 +0100

    fix buttons style in '/categories.blade.php'

--- resources/views/dashboard/categories.blade.php
+++ resources/views/dashboard/categories.blade.php
@@ -26,8 +26,11 @@
                                     </div>
                                 </td>
                                 <td>
-                                    <button>Delete</button>
-                                    <button>Edit</button>
+                                    <div class="buttons">
+                                        <button>
+                                            <svg viewBox="0 0 1024 1024"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g stroke-linecap="round" stroke-linejoin="round"></g><g><path d="M160 256H96a32 32 0 0 1 0-64h256V95.936a32 32 0 0 1 32-32h256a32 32 0 0 1 32 32V192h256a32 32 0 1 1 0 64h-64v672a32 32 0 0 1-32 32H192a32 32 0 0 1-32-32V256zm448-64v-64H416v64h192zM224 896h576V256H224v640zm192-128a32 32 0 0 1-32-32V416a32 32 0 0 1 64 0v320a32 32 0 0 1-32 32zm192 0a32 32 0 0 1-32-32V416a32 32 0 0 1 64 0v320a32 32 0 0 1-32 32z"></path></g></svg>
+                                        </button>
+                                    </div>
                                 </td>
                             </tr>
                         @endforeach
