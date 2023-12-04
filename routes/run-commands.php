cd94ec948146f35db782c12419d43b47f02f2831
hixvm <hixvmx@gmail.com>
2023-12-04 22:17:54 +0100

    update 'composer-install' command router

--- routes/run-commands.php
+++ routes/run-commands.php
@@ -1,12 +1,24 @@
 <?php
 use Illuminate\Support\Facades\Artisan;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 Route::prefix('/run/command')->group(function() {
-    Route::get('/composer-install', function() {
-        Artisan::call('composer:install');
-        return 'Running composer install.';
+    // Route::get('/composer-install', function() {
+    //     Artisan::call('composer:install');
+    //     return 'Running composer install.';
+    // });
+    Route::get('/composer-install', function () {
+        $process = new Process(['composer', 'install']);
+        
+        try {
+            $process->mustRun();
+            return 'Composer install completed successfully.';
+        } catch (ProcessFailedException $exception) {
+            return 'Composer install failed: ' . $exception->getMessage();
+        }
     });
-    
+
     Route::get('/migrate', function() {
         Artisan::call('migrate');
         return 'Migrating database.';
