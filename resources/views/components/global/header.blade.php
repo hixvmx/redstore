6eb88417b80c01620d712682596911d32b9b77eb
hixvm <hixvmx@gmail.com>
2023-12-09 20:21:39 +0100

    re-style header

--- resources/views/components/global/header.blade.php
+++ resources/views/components/global/header.blade.php
@@ -71,6 +71,28 @@
                         </div>
                     </div>
                 </li>
+            @else
+                <li class="header__li">
+                    <div class="header__user__dropdown">
+                        <div class="img__icon" style="background: transparent;">
+                            <svg viewBox="0 0 24 24" fill="none"><g stroke-width="0"></g><g stroke-linecap="round" stroke-linejoin="round"></g><g> <circle cx="12" cy="9" r="3" stroke="#898989" stroke-width="1.5"></circle> <path d="M17.9691 20C17.81 17.1085 16.9247 15 11.9999 15C7.07521 15 6.18991 17.1085 6.03076 20" stroke="#898989" stroke-width="1.5" stroke-linecap="round"></path> <path d="M7 3.33782C8.47087 2.48697 10.1786 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 10.1786 2.48697 8.47087 3.33782 7" stroke="#898989" stroke-width="1.5" stroke-linecap="round"></path> </g></svg>
+                        </div>
+                        <div class="user__dropdown">
+                            <div class="dropdown_body">
+                                <a class="a" href="/login">
+                                    <div>
+                                        <span>تسجيل الدخول</span>
+                                    </div>
+                                </a>
+                                <a class="a" href="/register">
+                                    <div>
+                                        <span>إنشاء حساب</span>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </li>
             @endif
             <li class="header__li">
                 <div class="header__publish__btn">
@@ -90,18 +112,6 @@
                     </a>
                 </div>
             </li>
-            @if (!$authCheck)
-            <li class="header__li">
-                <div class="header__login__btn">
-                    <a href="/login">
-                        <div>
-                            <i class="icon-login"></i>
-                            <span>دخول</span>
-                        </div>
-                    </a>
-                </div>
-            </li>
-            @endif
         </ul>
         <div class="header__logo">
             <a href="/">
