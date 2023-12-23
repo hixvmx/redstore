6e63feaabee85e900b048e1d51ef235c2b02f11a
hixvm <hixvmx@gmail.com>
2023-12-23 15:40:38 +0100

    integrate 'addtomyfavorites' in '/views/ad.blade.php'

--- resources/views/ad.blade.php
+++ resources/views/ad.blade.php
@@ -4,6 +4,8 @@
     <link rel="stylesheet" href="{{ asset('css/components/header.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/components/footer.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/ad.css') }}" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <script src="{{ asset('js/useAxios.js') }}"></script>
 @endsection
 
 @section('content')
@@ -53,6 +55,17 @@
                             </div>
                             <div class="image__img">
                                 <img src="{{ $ad->image }}" alt="{{ $ad->title }}" onclick="showPreviewImage('{{ $ad->image }}')" />
+                                @if (Auth::check())
+                                <button onclick="addToMyFavorites({{ $ad->id }})" class="favoriteBtn"><svg viewBox="0 0 24 24" fill="none">
+                                        <g stroke-width="0"></g>
+                                        <g stroke-linecap="round" stroke-linejoin="round"></g>
+                                        <g>
+                                            <path
+                                                d="M4.45067 13.9082L11.4033 20.4395C11.6428 20.6644 11.7625 20.7769 11.9037 20.8046C11.9673 20.8171 12.0327 20.8171 12.0963 20.8046C12.2375 20.7769 12.3572 20.6644 12.5967 20.4395L19.5493 13.9082C21.5055 12.0706 21.743 9.0466 20.0978 6.92607L19.7885 6.52734C17.8203 3.99058 13.8696 4.41601 12.4867 7.31365C12.2913 7.72296 11.7087 7.72296 11.5133 7.31365C10.1304 4.41601 6.17972 3.99058 4.21154 6.52735L3.90219 6.92607C2.25695 9.0466 2.4945 12.0706 4.45067 13.9082Z"
+                                                stroke-width="2"></path>
+                                        </g>
+                                    </svg></button>
+                                @endif
                             </div>
                             @if ($ad->images)
                                 <div class="more__images flex">
@@ -280,6 +293,34 @@
         function hidePreviewImage() {
             previewImg.src = '';
             previewBox.style.display = 'none';
+        }
+        
+        
+        var csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
+        async function sendRequest(id) {
+            try {
+                const response = await axios.post('/addToMyFavorites', {id}, {
+                    headers : {
+                        "X-CSRF-TOKEN": csrfToken
+                    }
+                })
+
+                const data = await response.data;
+
+                if (data.status == 1) {
+                    alert('Success!');
+                }
+
+            } catch (error) {
+                //
+            }
+        }
+
+        function addToMyFavorites(id) {
+            if (id) {
+                sendRequest(id);
+            }
         }
     </script>
 @endsection
