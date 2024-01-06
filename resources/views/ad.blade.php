66a3c1ada6170b62644852a04116a7b85a8843b8
hixvm <hixvmx@gmail.com>
2024-01-06 14:13:19 +0100

    show product details

--- resources/views/ad.blade.php
+++ resources/views/ad.blade.php
@@ -81,6 +81,45 @@
                             @endif
                         </div>
                     </div>
+
+                    {{-- Product Details --}}
+                    <div class="details">
+                        <div class="details__head">
+                            <span>معلومات المنتج</span>
+                        </div>
+                        <div class="details__row">
+                            <table class="detailsTable">
+                                <tr>
+                                    <td>التصنيف الرئيسي</td>
+                                    <td class="que">{{ $ad->category->name }}</td>
+                                </tr>
+                                <tr>
+                                    <td>التصنيف الفرعي</td>
+                                    <td class="que">{{ $ad->sub_category->name }}</td>
+                                </tr>
+                                <tr>
+                                    <td>الدولة</td>
+                                    <td class="que">{{ $ad->country->name }}</td>
+                                </tr>
+                                <tr>
+                                    <td>المدينة</td>
+                                    <td class="que">{{ $ad->city->name }}</td>
+                                </tr>
+                                <tr>
+                                    <td>الثمن</td>
+                                    <td class="que">{{ $ad->price .' '. $ad->currency->name }}</td>
+                                </tr>
+                                <tr>
+                                    <td>نوع المنتج</td>
+                                    <td class="que">##</td>
+                                </tr>
+                                <tr>
+                                    <td>تاريخ النشر</td>
+                                    <td class="que">{{ $ad->created_at['date'] }}</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
                 </div>
                 <div class="ad__aside">
                     <div class="publisher">
@@ -123,6 +162,25 @@
                         </div>
                     </div>
 
+                    {{-- Categories --}}
+                    <div class="categories">
+                        <div class="categories__head">
+                            <span>التصنيفات</span>
+                        </div>
+                        <div class="categories__row">
+                            @if ($categories->count() > 0)
+                                @foreach ($categories as $category)
+                                    <a href="/search?category={{ $category->id }}">
+                                        <div class="category">
+                                            <h3>{{ $category->name }}</h3>
+                                            <span>{{ $category->total_ads }}</span>
+                                        </div>
+                                    </a>
+                                @endforeach
+                            @endif
+                        </div>
+                    </div>
+
                     {{-- sharing options --}}
                     <?php
                         $adUrl = url()->current();
@@ -142,25 +200,6 @@
                             <div><button onclick="window.open('{{$linkedin}}','_blank')"><svg viewBox="0 0 24 24" fill="none"><g stroke-width="0"></g><g stroke-linecap="round" stroke-linejoin="round"></g><g> <path d="M6.5 8C7.32843 8 8 7.32843 8 6.5C8 5.67157 7.32843 5 6.5 5C5.67157 5 5 5.67157 5 6.5C5 7.32843 5.67157 8 6.5 8Z" fill="#3741519e"></path> <path d="M5 10C5 9.44772 5.44772 9 6 9H7C7.55228 9 8 9.44771 8 10V18C8 18.5523 7.55228 19 7 19H6C5.44772 19 5 18.5523 5 18V10Z" fill="#3741519e"></path> <path d="M11 19H12C12.5523 19 13 18.5523 13 18V13.5C13 12 16 11 16 13V18.0004C16 18.5527 16.4477 19 17 19H18C18.5523 19 19 18.5523 19 18V12C19 10 17.5 9 15.5 9C13.5 9 13 10.5 13 10.5V10C13 9.44771 12.5523 9 12 9H11C10.4477 9 10 9.44772 10 10V18C10 18.5523 10.4477 19 11 19Z" fill="#3741519e"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M20 1C21.6569 1 23 2.34315 23 4V20C23 21.6569 21.6569 23 20 23H4C2.34315 23 1 21.6569 1 20V4C1 2.34315 2.34315 1 4 1H20ZM20 3C20.5523 3 21 3.44772 21 4V20C21 20.5523 20.5523 21 20 21H4C3.44772 21 3 20.5523 3 20V4C3 3.44772 3.44772 3 4 3H20Z" fill="#3741519e"></path> </g></svg></button></div>
                             <div><button onclick="window.open('{{$telegrame}}','_blank')"><svg viewBox="0 0 24 24" fill="none"><g stroke-width="0"></g><g stroke-linecap="round" stroke-linejoin="round"></g><g> <path fill-rule="evenodd" clip-rule="evenodd" d="M23.1117 4.49449C23.4296 2.94472 21.9074 1.65683 20.4317 2.227L2.3425 9.21601C0.694517 9.85273 0.621087 12.1572 2.22518 12.8975L6.1645 14.7157L8.03849 21.2746C8.13583 21.6153 8.40618 21.8791 8.74917 21.968C9.09216 22.0568 9.45658 21.9576 9.70712 21.707L12.5938 18.8203L16.6375 21.8531C17.8113 22.7334 19.5019 22.0922 19.7967 20.6549L23.1117 4.49449ZM3.0633 11.0816L21.1525 4.0926L17.8375 20.2531L13.1 16.6999C12.7019 16.4013 12.1448 16.4409 11.7929 16.7928L10.5565 18.0292L10.928 15.9861L18.2071 8.70703C18.5614 8.35278 18.5988 7.79106 18.2947 7.39293C17.9906 6.99479 17.4389 6.88312 17.0039 7.13168L6.95124 12.876L3.0633 11.0816ZM8.17695 14.4791L8.78333 16.6015L9.01614 15.321C9.05253 15.1209 9.14908 14.9366 9.29291 14.7928L11.5128 12.573L8.17695 14.4791Z" fill="#3741519e"></path> </g></svg></button></div>
                             <div><button onclick="window.open('{{$whatsapp}}','_blank')"><svg viewBox="0 0 24 24" fill="none"><g stroke-width="0"></g><g stroke-linecap="round" stroke-linejoin="round"></g><g> <path fill-rule="evenodd" clip-rule="evenodd" d="M3.50002 12C3.50002 7.30558 7.3056 3.5 12 3.5C16.6944 3.5 20.5 7.30558 20.5 12C20.5 16.6944 16.6944 20.5 12 20.5C10.3278 20.5 8.77127 20.0182 7.45798 19.1861C7.21357 19.0313 6.91408 18.9899 6.63684 19.0726L3.75769 19.9319L4.84173 17.3953C4.96986 17.0955 4.94379 16.7521 4.77187 16.4751C3.9657 15.176 3.50002 13.6439 3.50002 12ZM12 1.5C6.20103 1.5 1.50002 6.20101 1.50002 12C1.50002 13.8381 1.97316 15.5683 2.80465 17.0727L1.08047 21.107C0.928048 21.4637 0.99561 21.8763 1.25382 22.1657C1.51203 22.4552 1.91432 22.5692 2.28599 22.4582L6.78541 21.1155C8.32245 21.9965 10.1037 22.5 12 22.5C17.799 22.5 22.5 17.799 22.5 12C22.5 6.20101 17.799 1.5 12 1.5ZM14.2925 14.1824L12.9783 15.1081C12.3628 14.7575 11.6823 14.2681 10.9997 13.5855C10.2901 12.8759 9.76402 12.1433 9.37612 11.4713L10.2113 10.7624C10.5697 10.4582 10.6678 9.94533 10.447 9.53028L9.38284 7.53028C9.23954 7.26097 8.98116 7.0718 8.68115 7.01654C8.38113 6.96129 8.07231 7.046 7.84247 7.24659L7.52696 7.52195C6.76823 8.18414 6.3195 9.2723 6.69141 10.3741C7.07698 11.5163 7.89983 13.314 9.58552 14.9997C11.3991 16.8133 13.2413 17.5275 14.3186 17.8049C15.1866 18.0283 16.008 17.7288 16.5868 17.2572L17.1783 16.7752C17.4313 16.5691 17.5678 16.2524 17.544 15.9269C17.5201 15.6014 17.3389 15.308 17.0585 15.1409L15.3802 14.1409C15.0412 13.939 14.6152 13.9552 14.2925 14.1824Z" fill="#3741519e"></path> </g></svg></button></div>
-                        </div>
-                    </div>
-
-                    {{-- Categories --}}
-                    <div class="categories">
-                        <div class="categories__head">
-                            <span>التصنيفات</span>
-                        </div>
-                        <div class="categories__row">
-                            @if ($categories->count() > 0)
-                                @foreach ($categories as $category)
-                                    <a href="/search?category={{ $category->id }}">
-                                        <div class="category">
-                                            <h3>{{ $category->name }}</h3>
-                                            <span>{{ $category->total_ads }}</span>
-                                        </div>
-                                    </a>
-                                @endforeach
-                            @endif
                         </div>
                     </div>
                 </div>
